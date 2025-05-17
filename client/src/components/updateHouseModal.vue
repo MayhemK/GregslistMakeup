366c9439a8e78e7971c439f3cacb392400590655
Ivan <mayhemk57@gmail.com>
2025-05-16 23:41:24 -0600

    modal shows same form as create, just nee to hide the create submit button

--- client/src/components/updateHouseModal.vue
+++ client/src/components/updateHouseModal.vue
@@ -1,4 +1,6 @@
 <script setup>
+import HouseForm from './HouseForm.vue';
+
 
 </script>
 
@@ -8,11 +10,11 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Update House Listing</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          <HouseForm />
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
